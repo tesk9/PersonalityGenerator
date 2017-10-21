5e750d953a7fad5d957f977d7dcee78cfecf5695
Tessa Kelly <tessakkelly@gmail.com>
2017-10-21 16:00:19 -0700

    Add trait views

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 import Accessibility exposing (..)
 import Button
+import Dict
 import Layout
 import Questions
 
@@ -21,8 +22,8 @@ main =
 
 type Model
     = UnStarted
-    | Generating Questions.Question
-    | FullyBaked String
+    | Generating ( List String, List String ) Questions.Question
+    | FullyBaked ( List String, List String )
 
 
 model : Model
@@ -30,14 +31,39 @@ model =
     UnStarted
 
 
+traits : Dict.Dict String ( List String, List String )
+traits =
+    Dict.fromList
+        [ ( "A"
+          , ( [ "earnest", "reserved", "sensible", "patient", "responsible", "cautious" ]
+            , [ "fastidious", "over-earnest", "proud", "stubborn", "tense", "mischievous", "obsessive", "pessimistic" ]
+            )
+          )
+        , ( "B"
+          , ( [ "passionate", "active", "creative", "animal-loving", "flexible", "cheerful", "friendly", "optimistic" ]
+            , [ "irresponsible", "forgetful", "selfish", "lazy", "impatient", "unreliable", "\"going own way\"" ]
+            )
+          )
+        , ( "AB"
+          , ( [ "cool", "controlled", "rational", "sociable", "intelligent", "adaptable" ]
+            , [ "critical", "indecisive", "unforgiving", "two-faced", "aloof", "\"split personality\"" ]
+            )
+          )
+        , ( "O"
+          , ( [ "confident", "self-determined", "ambitious", "strong-willed", "intuitive", "agreeable", "competitive", "athletic" ]
+            , [ "unpredictable", "spiteful", "self-centered", "cold", "aggressive", "arrogant", "envious", "ruthless" ]
+            )
+          )
+        , ( "Rh negative", ( [ "smart", "high intuition", "fact-based" ], [ "easily frightened" ] ) )
+        , ( "Rh positive", ( [], [] ) )
+        ]
+
+
 bloodType : Questions.Question
 bloodType =
-    Questions.new "What is your blood type?"
-        [ ( "0", Just rhFactor )
-        , ( "A", Just rhFactor )
-        , ( "B", Just rhFactor )
-        , ( "AB", Just rhFactor )
-        ]
+    [ "A", "B", "AB", "O" ]
+        |> List.map (\key -> ( key, Just rhFactor ))
+        |> Questions.new "What is your blood type?"
 
 
 rhFactor : Questions.Question
@@ -85,37 +111,52 @@ view model =
                 [ case model of
                     UnStarted ->
                         changeStepButton "Start Generating" <|
-                            Generating bloodType
+                            Generating ( [], [] ) bloodType
 
-                    Generating question ->
-                        viewOptions question
+                    Generating traits question ->
+                        viewOptions traits question
 
-                    FullyBaked personality ->
-                        text personality
+                    FullyBaked ( goodTraits, badTraits ) ->
+                        div []
+                            [ h3 [] [ text "Best traits" ]
+                            , ul [] (List.map viewTrait goodTraits)
+                            , h3 [] [ text "Worst traits" ]
+                            , ul [] (List.map viewTrait badTraits)
+                            ]
                 ]
             ]
         ]
 
 
-viewOptions : Questions.Question -> Html Msg
-viewOptions question =
+viewOptions : ( List String, List String ) -> Questions.Question -> Html Msg
+viewOptions traits question =
     div []
         [ h3 [] [ text (Questions.directions question) ]
-        , div [] (List.map viewOption (Questions.options question))
+        , div [] (List.map (viewOption traits) (Questions.options question))
         ]
 
 
-viewOption : Questions.Option -> Html Msg
-viewOption ( option, nextQuestion ) =
+viewOption : ( List String, List String ) -> Questions.Option -> Html Msg
+viewOption ( goodTraits, badTraits ) ( option, nextQuestion ) =
     changeStepButton option <|
         case nextQuestion of
             Just question ->
-                Generating question
+                let
+                    ( newGoodTraits, newBadTraits ) =
+                        Dict.get option traits
+                            |> Maybe.withDefault ( [], [] )
+                in
+                Generating ( newGoodTraits ++ goodTraits, newBadTraits ++ badTraits ) question
 
             Nothing ->
-                FullyBaked "Here's your personality!"
+                FullyBaked ( goodTraits, badTraits )
 
 
 changeStepButton : String -> Model -> Html Msg
 changeStepButton text_ changeTo =
     Button.view text_ (ChangeStep changeTo)
+
+
+viewTrait : String -> Html msg
+viewTrait trait =
+    li [] [ text trait ]
